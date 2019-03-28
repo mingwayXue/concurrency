42d16369192280f06e5c571f376d6762d53d1054
mingwayXue <571025297@qq.com>
2019-03-28 23:27:28 +0800

    add deadlock demo code

---
+++ src/main/java/com/xue/concurrency/deadlock/DeadLockTest01.java
@@ -0,0 +1,54 @@
+package com.xue.concurrency.deadlock;
+
+import lombok.extern.slf4j.Slf4j;
+
+/** 死锁示例
+ * Created by Mingway on 2019/3/28.
+ */
+@Slf4j
+public class DeadLockTest01 implements Runnable {
+
+    public int flag = 1;
+    private static Object o1 = new Object(), o2 = new Object();
+
+    @Override
+    public void run() {
+        log.info("flag:{}", flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o2) {
+                    log.info("1");
+                }
+            }
+        }
+        if (flag == 0) {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (o1) {
+                    log.info("2");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DeadLockTest01 lock1 = new DeadLockTest01();
+        DeadLockTest01 lock2 = new DeadLockTest01();
+
+        lock1.flag = 1;
+        lock2.flag = 0;
+
+        // 产生死锁，o1等待o2的资源，o2等待o1的资源
+        new Thread(lock1).start();
+        new Thread(lock2).start();
+    }
+}
